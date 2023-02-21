f329ab7b2e3edbcc441559faef9f75b806fac1c2
alebe2004 <alebe2004@gmail.com>
2023-02-21 04:52:57 -0800

    task dgk

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -9,6 +9,7 @@
 int main(void)
 {
 	int n;
+
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	if (n > 0)
@@ -17,4 +18,5 @@ int main(void)
 		printf("%d is zero\n", n);
 	else
 		printf("%d is negative\n", n);
-	return (0)
+	return (0);
+}
