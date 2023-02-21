f618cefa531c3824f3eb90f606d0fc0918e90b9c
alebe2004 <alebe2004@gmail.com>
2023-02-21 04:44:15 -0800

    gdki

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -6,16 +6,16 @@
 
 /**
  * main - prints all single digit numbers of base 10 starting from 0,
- *     only using putchar and without char
-       variables.
-*
-* Return: Always 0.
-*/
+ * only using putchar and without char variables.
+ *
+ * Return: Always 0.
+ */
 int main(void)
 {
 	int num;
-	for (num =0; num < 10; num++)
+
+	for (num = 0; num < 10; num++)
 		putchar((num % 10) + '0');
-	puchar('\n');
-	return(0);
+	putchar('\n');
+	return (0);
 }
