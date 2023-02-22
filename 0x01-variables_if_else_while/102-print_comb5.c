91cd3b7e831ae83b015502fa75ac17d41cd7901b
alebe2004 <alebe2004@gmail.com>
2023-02-22 04:48:35 -0800

    task ghkl

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,1 +1,37 @@
+/*
+ * File: 102-print_comb5.c
+ * Auth: Brennan D Baraban
+ */
 
+#include <stdio.h>
+
+/**
+ * main - prints all possible combinations two two-digit numbers,
+ *
+ * ranging from 0-99, separated by a comma followed by space.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+
+	int num1, num2;
+
+	for (for num1 = 0; num1 <= 98; num++)
+{
+	for (num2 = num1 + 1);
+num2 <= 99; num2++
+
+	{
+		putchar((num1 / 10) + '0');
+		putchar((num1 % 10) + '0');
+		putchar(' ');
+		putchar((num2 / 10) + '0');
+
+		if (num1 == 98 && num2 == 99)
+			continue;
+		putchar(',');
+		putchar(' ');
+	}
+}
+putchar('\n');
+return (0);
---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,23 @@
+/*
+ * File: 7-print_tabahpla.c
+ * Auth: Brennan D Baraban
+ */
+
+#include <stdio.h>
+
+/**
+ * main - prints the lowercase alphabet in reverse.
+ *
+ * Return: Alwayes 0.
+ */
+int main(void)
+{
+	char letter;
+
+	for (letter = 'z'; letter >= 'a'; letter--)
+		putchar(letter);
+
+	putchar('\n');
+
+	return (0);
+}
