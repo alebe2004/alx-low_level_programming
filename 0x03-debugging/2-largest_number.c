2d4067e54543421daf01859a25695022375a239b
alebe2004 <alebe2004@gmail.com>
2023-02-26 07:10:46 -0800

    task bert

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -5,22 +5,24 @@
  * @a: first integer
  * @b: second integer
  * @c: thired integer
- * Returen: largest
+ * Return: largest number
  */
 
 int largest_number(int a, int b, int c)
-{
-	int largest;
+{int largest;
 
 	if (a > b && b > c)
 	{
 		largest = a;
 	}
 	else if (b > a && a > c)
+	{
 		largest = b;
 }
 else
 {
 	largest = c;
+}
+
 return (largest);
 }
