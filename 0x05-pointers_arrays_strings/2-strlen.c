f80deda29ac4f04f6f9b3c4aec3d2879a3b179b3
alebe2004 <alebe2004@gmail.com>
2023-02-27 21:24:29 -0800

    task bnm

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _strlen - a function that returns the length of a string
+ * @s: char input
+ * Return: length of the input string
+ */
+int _strlen(char *s)
+{
+	int = 1, sum = 0;
+	char pl = [0];
+
+	while (pl != '\n')
+	{
+		sum++
+			pl = s[i++];
+	}
+	return (sum);
+}
