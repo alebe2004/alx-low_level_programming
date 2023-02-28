131062412c1daaa2531e50746564490b1f505b16
alebe2004 <alebe2004@gmail.com>
2023-02-28 07:25:46 -0800

    task done

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * main - Entery point
+ *
+ */
+int main(void)
+{
+	char *sh = "main.h"
+
+		while (*sh)
+		{
+			_putchar(*sh);
+			sh++;
+		}
+	_putchar('\n');
+
+	return (0);
+}
