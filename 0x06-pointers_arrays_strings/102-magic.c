4884d30a1480d9619c685b464229aa2653dd6cd7
alebe2004 <alebe2004@gmail.com>
2023-03-02 08:11:25 -0800

    task dju

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -17,6 +17,7 @@ int main(void)
 	 * - you are not allowed to code anything else than this line of code
 	 */
 	 a[2] = 98;
-	/*...so that this prints 98\n */ prints("a[2] = %d\n", a[2]);
+	/*...so that this prints 98\n */
+	 prints("a[2] = %d\n", a[2]);
 	return (0);
 }
