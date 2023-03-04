0af768cb8aa0ab159bb6795dca962fc728faa021
alebe2004 <alebe2004@gmail.com>
2023-03-04 07:50:02 -0800

    task bhu

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,17 +1,21 @@
 #include "main.h"
+
 /**
- * main - Entery point
- *
- */
+  * main - Entry point
+  *
+  * Return: Always 0
+  */
 int main(void)
 {
-	char *sh = "main.h"
-
-		while (*sh)
-		{
-			_putchar(*sh);
-			sh++;
-		}
+	_putchar('H');
+	_putchar('o');
+	_putchar('l');
+	_putchar('b');
+	_putchar('e');
+	_putchar('r');
+	_putchar('t');
+	_putchar('o');
+	_putchar('n');
 	_putchar('\n');
 
 	return (0);
