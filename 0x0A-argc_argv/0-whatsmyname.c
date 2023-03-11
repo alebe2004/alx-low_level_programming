dbc7a2216342525c45befe163babc5b966e181a9
alebe2004 <alebe2004@gmail.com>
2023-03-11 04:09:36 -0800

    alughk

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,20 @@
+#include "main.h"
+ #include <stdio.h>
+
+/**
+ * main - prints the name of the program
+ * @argc: Number of command line arguments
+ * @argv: Arry name
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	printf("%s\nn", argv[argc * 0]);
+
+	return (0);
+}
+
+
+
+
---
+++ 0x0A-argc_argv/main.h
@@ -0,0 +1,10 @@
+#ifndef MAIN_H
+#define HOLBERTON_H
+
+int _putchar(char c);
+
+#endif
+
+
+
+
