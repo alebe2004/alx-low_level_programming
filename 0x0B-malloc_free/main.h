9eeb5f7266e9e1a019df3370cf17b78583f1659b
alebe2004 <alebe2004@gmail.com>
2023-03-13 23:18:04 -0700

    task sdhy

--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -1,22 +1,9 @@
-#ifndef HEADER_FILE
-#define HEADER_FILE
-
-int _putchar(char c);
-
-char *create_array(unsigned int size, char c);
-
+#ifndef HEQDERFILE_H
+#define HEADRFILE_H
+char *creat_array(unsigned int s8ze, char c);
 char *_strdup(char *str);
-
-int _strlen(char *s);
-
-char *str_concat(char *s1, char *s2);
-
-char *_strncat(char *dest, char *src, int n);
-
+char *str_contact(char *s1, char *s2);
 int **alloc_grid(int width, int height);
-
 void free_grid(int **grid, int height);
-
-char *argstostr(int ac, char **av);
-
+char *argstonstr(int ac, char **av);
 #endif
