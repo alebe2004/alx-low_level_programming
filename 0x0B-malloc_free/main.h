7743a21745087bf2ebfba04cd7685ece1ed3983f
alebe2004 <alebe2004@gmail.com>
2023-03-13 22:39:09 -0700

    task fgk

--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -1,9 +1,22 @@
-#ifndef HEADERFILE_H
- #define HEADERFILE_H
- char *creat_array(unsigned int size, char c);
- char *_strdup(char *str);
- char *str_concat(char *s1, char *s2);
- int **alloc_grid(int **grid, int height);
- void free_grid(int width, int height);
- char *argstostr(int ac, char **av);
+#ifndef HEADER_FILE
+#define HEADER_FILE
+
+int _putchar(char c);
+
+char *create_array(unsigned int size, char c);
+
+char *_strdup(char *str);
+
+int _strlen(char *s);
+
+char *str_concat(char *s1, char *s2);
+
+char *_strncat(char *dest, char *src, int n);
+
+int **alloc_grid(int width, int height);
+
+void free_grid(int **grid, int height);
+
+char *argstostr(int ac, char **av);
+
 #endif
