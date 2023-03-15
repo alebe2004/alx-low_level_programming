2178c8146bd12359c3dc213c7d8ae5c27e92be9b
alebe2004 <alebe2004@gmail.com>
2023-03-15 03:24:41 -0700

    update ghj

--- 0x09-static_libraries/6-abs.c
+++ 0x09-static_libraries/6-abs.c
@@ -1,5 +1,20 @@
-#include "main.h"
+/*
+ * File: 6-abs.c
+ * Auth: Brennan D Baraban
+ */
+
+#include "holberton.h"
+
+/**
+ * _abs - Computes the absolute value of an integer.
+ * @n: The integer to be computed.
+ *
+ * Return: The absolute value of the integer.
+ */
 int _abs(int n)
 {
-	return (0);
+	if (n >= 0)
+		return (n);
+	else
+		return (-n);
 }
