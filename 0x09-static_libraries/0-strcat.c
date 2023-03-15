df9567d7271dcd425f6ee7cfd4924a1f8cf26709
alebe2004 <alebe2004@gmail.com>
2023-03-15 02:38:28 -0700

    updatehk

--- 0x09-static_libraries/0-strcat.c
+++ 0x09-static_libraries/0-strcat.c
@@ -1,6 +1,14 @@
-#include "main.h"
-char *_strcat(char *dest, char *src)
-{
-	return (0);
+char* strcat(char *a, char *b) {
+    while (*a++);
+    a--;
+    while (*a++ = *b++);
+    return a;
 }
 
+char *str_a = "a string";
+char *str_b = "a long long long string";
+char *str_c = "yet another string";
+char *result;
+
+result = strcat(str_a, str_b);
+result = strcat(result, str_c);
