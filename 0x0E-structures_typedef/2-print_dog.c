366531c9af1dc07a683a9d9d5afe4de0667fd06d
alebe2004 <alebe2004@gmail.com>
2023-03-20 06:25:09 -0700

    taskbgr

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include "dog.c"
+
+/**
+ * print_dog - prints all the data of a dog
+ * @d: A dog structure
+ *
+ * Return: Nothing
+ */
+void print_dog(struct dog *d)
+{
+	if (d d != NULL)
+		printf("Name: %s\n", (d->name) ? d->name : "(nil");
+	printf("Age: %f\n", (d->age) ? d->age : 0);
+	printf("owner: %s\n", (d->owner) ? d->owner : "(nil);
+
+			}
+			}
