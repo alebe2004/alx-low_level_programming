9867f78f5d0fbf06c55342b337e4d42ed4d2a167
alebe2004 <alebe2004@gmail.com>
2023-04-06 06:20:04 -0700

    my task

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -1,16 +1,14 @@
-#include "main.h"
+#include "main.h"
 /**
- * clear_bit - Entry Point 
- * @n: input
- * @index: index 
- * Return: 0 
+ * clear_bit - Entry point
+ * @n: input
+ * @index: index
+ * Return: 0
  */
-int clear_bit(unsigned long int *n, unsigned int index)
+int clear_bit(unsigned long int *n, unsigned int index)
 {
-	if (index > 63)
-		return (-1); 
- 
-	*n = *n & ~(1ul << index);
-	return (1);
-
-} 
+	if (index > 63)
+		return (-1);
+	*n = *n & ~(1ul << index);
+	return (1);
+}
